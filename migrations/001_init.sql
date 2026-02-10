a0335aea25e6dbd09a5a6fcc1814000dc79a43f0
Swapnil Narwade <swapnil.narwade3@gmail.com>
2026-02-11 05:19:14 +0530

    renamed to 001_init_envhub.sql

--- migrations/001_init.sql
+++
@@ -1,8 +0,0 @@
--- Initial schema setup
--- This will be populated in Phase 1.2
-
--- Enable UUID extension
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Placeholder for initial tables
--- Will be added in database design phase
