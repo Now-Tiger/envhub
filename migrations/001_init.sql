4b5f0542898551b2d7a04164be1359e636a189ce
Swapnil Narwade <swapnil.narwade3@gmail.com>
2026-02-11 00:11:48 +0530

    Initial migration sql file

---
+++ migrations/001_init.sql
@@ -0,0 +1,8 @@
+-- Initial schema setup
+-- This will be populated in Phase 1.2
+
+-- Enable UUID extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Placeholder for initial tables
+-- Will be added in database design phase
